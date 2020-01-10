09ecf59a28e123a9ef31f3dce4ed65eb892c6a2e
yaolong <xyaolong@hotmail.com>
2020-01-10 10:37:30 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
+Author: xyaolong
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+1. Modify default IP
+sed -i 's/192.168.1.1/192.168.2.5/g' openwrt/package/base-files/files/bin/config_generate
